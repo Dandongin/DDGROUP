dbafd694cb1d05727edd125e9500291e484b12a0
Dandongin <sutian300@gmail.com>
2026-09-06 19:48:31 +0900

    Create config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCxQ8kYN_VL6-_8oqhrUv3TjV-Yjpimo00",
+  authDomain: "dd-group-2c8e8.firebaseapp.com",
+  projectId: "dd-group-2c8e8",
+  storageBucket: "dd-group-2c8e8.firebasestorage.app",
+  messagingSenderId: "35622249443",
+  appId: "1:35622249443:web:ee7e139f1ca0e3792af6d6"
+};
